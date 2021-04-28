3f61cf5a8d46fc5537e4ba29f7e588ec0d642b34
Olaf Mudde <olaf.mudde@xs4all.nl>
2021-04-29 00:15:41 +0200

    Add submit button

---
+++ src/Button/Submit.ts
@@ -0,0 +1,46 @@
+import Node from "../../node_modules/mudde-node/src/node"
+import ButtonAbstract from "../Form/ButtonAbstract"
+
+export default class Submit extends ButtonAbstract {
+
+   constructor(config: any) {
+      super()
+      this.configuring(config)
+   }
+
+   getDefaultConfig() {
+      return {
+         ...super.getDefaultConfig(),
+      }
+   }
+
+   coreHTMLInput(id: string, name: string, language: string): Node {
+      var attributes: any = {
+         type: 'submit',
+         class: 'btn btn-primary',
+         onclick: `javascript:
+         b var data = {};
+         Array.from(document.forms[0].elements).forEach(element => {
+             if (element.name) {
+                 if (element.type === 'file') {
+                     tmpData = [];
+                     Array.from(element.files).forEach(element => {
+                         tmpData.push(element.name)
+                     });
+                     data[element.name] = tmpData
+                 } else {
+                     data[element.name] = element.value
+                 }
+             }
+         });
+         alert(JSON.stringify({valid: document.forms[0].checkValidity(),...data}, null, 4));
+         return false`,
+         value: this.label
+      }
+
+      var element: Node = new Node('input', attributes)
+
+      return element
+   }
+
+}
