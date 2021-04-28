4447e4ca55ed66e155e23f930a0f466e57bbe9bb
Olaf Mudde <olaf.mudde@xs4all.nl>
2021-04-28 22:32:52 +0200

    Add Array Data

---
+++ src/Data/Array.ts
@@ -0,0 +1,54 @@
+import DataAbstract from "../Form/DataAbstract";
+import DataEvent from "../Form/DataEvent";
+import Form from "../Form/Form";
+
+export default class Array extends DataAbstract {
+
+   private _data: any = {}
+   private _currentData: any = {}
+
+   constructor(config: any, form: Form) {
+      super(form)
+      this.configuring(config)
+   }
+
+   getDefaultConfig() {
+      return {
+         data: {}
+      }
+   }
+
+   update() {
+
+   }
+
+   connect(): void {
+   }
+
+   restore(id: string): any {
+      this._currentData[id] = this._data[id]
+   }
+
+   get(id: string): any {
+      var event = new DataEvent(this, DataAbstract.DATA_PRE_GET, id)
+      this.notify(event)
+
+      var value = this._currentData[id] ? this._currentData[id] : this._data[id]
+
+      var event = new DataEvent(this, DataAbstract.DATA_POST_GET, id)
+      this.notify(event)
+
+      return value;
+   }
+
+   set(id: string, value: any): void {
+      var event = new DataEvent(this, DataAbstract.DATA_PRE_SET, id)
+      this.notify(event)
+
+      this._currentData[id] = value
+
+      var event = new DataEvent(this, DataAbstract.DATA_POST_SET, id)
+      this.notify(event)
+   }
+
+}
---
+++ src/Form/DataEvent.ts
@@ -0,0 +1,18 @@
+import Event from "./Event";
+
+export default class DataEvent extends Event {
+   
+   private _id?:string
+
+   constructor(source:any, event:number, id:string){
+      super(source,event)
+      this._id = id
+   }
+
+   get id():string{
+      if(this._id === undefined) throw new Error('Id not set!')
+
+      return this._id
+   }
+
+}
