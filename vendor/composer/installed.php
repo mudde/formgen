e38eca7b52d21428ad2153073517c14cd325f6df
mudde <olaf.mudde@xs4all.nl>
2021-09-03 00:41:47 +0200

    Add compoer.json update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '088663041e8727917afcd1a993e293919e8efcc8',
+    'name' => 'mudde/formgen',
+  ),
+  'versions' => 
+  array (
+    'mudde/core' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '2edfae00782e7a709c074b6667e9a9a9b38cc29c',
+    ),
+    'mudde/formgen' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '088663041e8727917afcd1a993e293919e8efcc8',
+    ),
+  ),
+);
